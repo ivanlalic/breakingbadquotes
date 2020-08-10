1eb3ebea27ca9cff1312ed9a0bbb406aaac8bc82
ivanlalic <ivanlalic@gmail.com>
2020-08-10 00:03:12 -0300

    Updates

---
+++ precache-manifest.84de32d02a26e9a3a19d8c60cd99c187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bebebf5d6294b8710857810eeb4a0b3",
+    "url": "/breakingbadquotes/index.html"
+  },
+  {
+    "revision": "f544387439abecb4a427",
+    "url": "/breakingbadquotes/static/css/main.bbb1878b.chunk.css"
+  },
+  {
+    "revision": "bf7e541b29b38394ba5e",
+    "url": "/breakingbadquotes/static/js/2.c85fab59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbadquotes/static/js/2.c85fab59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f544387439abecb4a427",
+    "url": "/breakingbadquotes/static/js/main.ca7e06eb.chunk.js"
+  },
+  {
+    "revision": "59b1672c51765ec94db8",
+    "url": "/breakingbadquotes/static/js/runtime-main.228a69a3.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/breakingbadquotes/static/media/logo.427a4adc.svg"
+  }
+]);
